a425acda6616aab459dd71997c36b2d49366ba6b
RDG209 <rgaeta209@gmail.com>
2024-10-18 10:04:50 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	var num int = 42
+	var pi float64 = 3.14
+	var isGoFun bool = true
+	var name string = "GoLang"
+
+	fmt.Println("Integer:", num)
+	fmt.Println("Float:", pi)
+	fmt.Println("Boolean:", isGoFun)
+	fmt.Println("String:", name)
+}
