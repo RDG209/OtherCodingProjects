17fd191ec3deb9d4d85b04a3a71184d2796308ee
RDG209 <rgaeta209@gmail.com>
2024-10-19 09:16:06 -0700

    Add files via upload

---
+++ hello.scm
@@ -0,0 +1,16 @@
+
+; Print "Hello, World!"
+(display "Hello, World!")
+(newline)
+
+; Defining primitive types
+(define num 42)
+(define pi 3.1415)
+(define schemeTheGoat #t)
+(define name "Prolog")
+
+; Output the values
+(display "Integer: ") (display num) (newline)
+(display "Float: ") (display pi) (newline)
+(display "Boolean: ") (display schemeTheGoat) (newline)
+(display "String: ") (display name) (newline)
