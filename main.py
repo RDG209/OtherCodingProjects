cc910c18f3102af377dd430453e86321bfab42f9
RDG209 <rgaeta209@gmail.com>
2024-10-19 09:22:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+print("Hello, World!")
+
+num = 42
+pi = 3.14
+isPythonFun = True
+name = "Python"
+
+print("Integer: ", num)
+print("Float: ", pi)
+print("Boolean: ", isPythonFun)
+print("String: ", name)
+
